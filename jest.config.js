7c7fe063deb4826173e99652bac9c1999d692366
jasonmcalpin <golmspace@gmail.com>
2025-07-10 22:51:02 -0400

    fix(es6): convert typescript features back to es6 for import exports

--- jest.config.js
+++ jest.config.js
@@ -77,18 +77,18 @@ const config = {
   // ],
 
   // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
-  //   "mjs",
-  //   "cjs",
-  //   "jsx",
-  //   "ts",
-  //   "mts",
-  //   "cts",
-  //   "tsx",
-  //   "json",
-  //   "node"
-  // ],
+  moduleFileExtensions: [
+    "js",
+    // "mjs",
+    // "cjs",
+    // "jsx",
+    // "ts",
+    // "mts",
+    // "cts",
+    // "tsx",
+    // "json",
+    // "node"
+  ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
@@ -128,8 +128,8 @@ const config = {
 
   // A list of paths to directories that Jest should use to search for files in
   roots: [
-    "<rootDir>/src",
-    "<rootDir>/tests"
+    "<rootDir>/frontend/src",
+    "<rootDir>/frontend/tests"
   ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
