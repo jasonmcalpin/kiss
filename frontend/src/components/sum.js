b33c518fe74a84fde98256dedee4e188b863659d
Jason McAlpin <golmsapce@gmail.com>
2025-07-20 21:19:36 -0400

    feat(refactor): convert to TypeScript for better code quality.

--- frontend/src/components/sum.js
+++
@@ -1,7 +0,0 @@
-function sum(a, b) {
-  return a + b;
-}
-function subtract(a,b) {
-  return a - b;
-}
-export {sum, subtract};
---
+++ frontend/tests/sum.test.ts
@@ -0,0 +1,5 @@
+const sum = require('../frontend/src/sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
