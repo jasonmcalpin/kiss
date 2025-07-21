4e0f2703a5188d08b0e8e1a0361e1295d0a74825
Jason McAlpin <golmsapce@gmail.com>
2025-07-20 22:41:46 -0400

    feat(tests): jest updated and working in WSL on windows. works normally on OSX or linux. Windows does not properly install binaries in .bin folder

--- frontend/tests/sum.test.ts
+++ frontend/tests/sum.test.ts
@@ -1,5 +1,5 @@
-const sum = require('../frontend/src/sum');
+const { sum } = require('../src/components/sum');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+  expect( sum(1, 2) ).toBe(3);
 });
