e42cebcdd63d830ac1f3182db767fe2750d1635e
Jason McAlpin <golmspace@gmail.com>
2025-07-21 05:09:26 -0400

    wip(styling): this is adding vite and tailwind. this is a test to see if it will help or hurt the project. the goal is to keep it simple and light.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./frontend/public/index.html",
+    "./frontend/src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
